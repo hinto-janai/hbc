9385c632165e0a7d9188a316fd2c1baa36d2276d
hinto.janaiyo <hinto.janaiyo@protonmail.com>
2022-07-24 16:58:19 -0400

    safety: readd unset -f . : [, but outside of posix mode.

--- main.sh
+++ main.sh
@@ -328,7 +328,8 @@ POSIXLY_CORRECT= || exit 90
 \unset -f arch base64 basename cat chcon chgrp chmod chown chroot cksum comm cp csplit cut date dd df dir dircolors dirname du echo env expand expr factor false fmt fold groups head hostid hostname id install join kill link ln logname ls md5sum mkdir mkfifo mknod mktemp mv nice nl nohup nproc numfmt od paste pathchk pinky pr printenv printf ptx pwd readlink realpath rm rmdir runcon seq shred shuf sleep sort split stat stdbuf stty sum tac tail tee test timeout touch tr true truncate tsort tty uname unexpand uniq unlink uptime users vdir wc who whoami yes || exit 92
 \unalias -a || exit 93
 unset POSIXLY_CORRECT || exit 94
-set -eo pipefail || exit 95
+unset -f . : [ || exit 95
+set -eo pipefail || exit 96
 EOM
 )
 # lib
